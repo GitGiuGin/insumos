e61f0eb25fd34140d5299f37e618276d6f37be33
GitGiuGin <jmacross_1995@hotmail.com>
2024-06-11 12:07:08 -0400

    BD copaida a package db

---
+++ db/insumos.sql
@@ -0,0 +1,533 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 11-06-2024 a las 18:01:33
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `insumos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `alquiler`
+--
+
+CREATE TABLE `alquiler` (
+  `id` int(11) NOT NULL,
+  `id_cliente` int(11) DEFAULT NULL,
+  `id_usuario` int(11) DEFAULT NULL,
+  `num_comprobante` varchar(45) DEFAULT NULL,
+  `dias` int(11) DEFAULT NULL,
+  `fecha` date DEFAULT NULL,
+  `total` decimal(11,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categoria`
+--
+
+CREATE TABLE `categoria` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `categoria`
+--
+
+INSERT INTO `categoria` (`id`, `nombre`) VALUES
+(3, 'Dispositivos médicos'),
+(4, 'Equipos médicos'),
+(5, 'Insumos médicos'),
+(2, 'Mobiliario'),
+(1, 'Repuestos');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(45) NOT NULL,
+  `apellido` varchar(45) NOT NULL,
+  `tipo_documento` varchar(45) NOT NULL,
+  `num_documento` varchar(45) NOT NULL,
+  `direccion` varchar(255) DEFAULT NULL,
+  `telefono` varchar(20) NOT NULL,
+  `correo` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `cliente`
+--
+
+INSERT INTO `cliente` (`id`, `nombre`, `apellido`, `tipo_documento`, `num_documento`, `direccion`, `telefono`, `correo`) VALUES
+(1, 'Gabriel Alessandro', 'Rada Flores', 'Carnet de identidad', '9909976', 'Av. Armentia #1243 Zona Central', '76506497', 'gabriel.rada@gmail.com'),
+(2, 'Oscar', 'Ramirez Coaquira', 'Carnet de identidad', '6861186', 'Calle 21 de Septiembre #2325 Zona 1ro de Mayo', '79119763', 'oscar.ramirez@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `compra`
+--
+
+CREATE TABLE `compra` (
+  `id` int(11) NOT NULL,
+  `id_proveedor` int(11) DEFAULT NULL,
+  `id_usuario` int(11) DEFAULT NULL,
+  `num_comprobante` varchar(45) DEFAULT NULL,
+  `fecha` date DEFAULT NULL,
+  `total` decimal(11,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalle_alquiler`
+--
+
+CREATE TABLE `detalle_alquiler` (
+  `id` int(11) NOT NULL,
+  `id_alquiler` int(11) DEFAULT NULL,
+  `id_producto` int(11) DEFAULT NULL,
+  `cantidad` int(11) DEFAULT NULL,
+  `descuento` decimal(11,2) DEFAULT NULL,
+  `precio` decimal(11,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalle_compra`
+--
+
+CREATE TABLE `detalle_compra` (
+  `id` int(11) NOT NULL,
+  `id_compra` int(11) DEFAULT NULL,
+  `id_producto` int(11) DEFAULT NULL,
+  `cantidad` int(11) DEFAULT NULL,
+  `precio` decimal(11,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalle_venta`
+--
+
+CREATE TABLE `detalle_venta` (
+  `id` int(11) NOT NULL,
+  `id_venta` int(11) DEFAULT NULL,
+  `id_producto` int(11) DEFAULT NULL,
+  `cantidad` int(11) DEFAULT NULL,
+  `descuento` decimal(11,2) DEFAULT NULL,
+  `precio` decimal(11,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pais`
+--
+
+CREATE TABLE `pais` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `pais`
+--
+
+INSERT INTO `pais` (`id`, `nombre`) VALUES
+(1, 'Estados Unidos'),
+(2, 'China'),
+(3, 'Japon'),
+(4, 'India');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE `producto` (
+  `id` int(11) NOT NULL,
+  `codigo` varchar(45) DEFAULT NULL,
+  `nombre` varchar(255) DEFAULT NULL,
+  `id_categoria` int(11) DEFAULT NULL,
+  `id_pais` int(11) DEFAULT NULL,
+  `precio_compra` decimal(11,2) DEFAULT NULL,
+  `precio_venta` decimal(11,2) DEFAULT NULL,
+  `cantidad` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `producto`
+--
+
+INSERT INTO `producto` (`id`, `codigo`, `nombre`, `id_categoria`, `id_pais`, `precio_compra`, `precio_venta`, `cantidad`) VALUES
+(6, 'SSP218DDA-ELR', 'SILLA DE RUEDA CON POZA BRAZO DESMONTABLE Y P', 4, 1, 1859.62, 2401.20, 1),
+(15, '71419360083', 'Artrosamin Crema Frasco X 227G Grande', 5, 1, 131.31, 144.93, 2),
+(16, 'INTCO-0001', 'GUANTES DE VINIL TALLA PEQUEÑA', 5, 3, 30.11, 43.01, 10),
+(17, '0714193620029 ', 'INMUNE COMPLEX X 60 CAPSULAS - INTERFARMA', 5, 1, 98.00, 129.00, 10),
+(18, 'ESTUCHE-DDP', 'ESTUCHE DE DISECCION DE 13 PIEZAS CON LAMINAS - DADDY PRO\r\n', 4, 3, 150.00, 167.04, 5),
+(19, 'B-35-1', 'CAMA INFANTIL DE 1 MOVIMIENTO CON BARANDAS Y RUEDAS - AMD', 2, 3, 3206.98, 4102.08, 2),
+(20, 'F-33', 'MESA DE ALIMENTACIÓN CON ALTURA REGULABLE - AMD', 2, 4, 863.52, 1134.55, 3),
+(21, 'HEM-CL24', 'ABRAZADERA PARA TENSIÓMETRO AUTOMÁTICO-OMRON', 1, 1, 153.88, 189.93, 4),
+(22, 'OXM-INF-001', 'OXÍMETRO DE PULSO ADULTO E INFANTIL', 3, 2, 150.00, 185.00, 10),
+(23, '603BK', 'ESTETOSCOPIO CON CAMPANA DOBLE COLOR NEGRO AD DE 56CM-ADC', 3, 1, 365.63, 418.00, 6);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `proveedor`
+--
+
+CREATE TABLE `proveedor` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(45) DEFAULT NULL,
+  `num_documento` varchar(45) DEFAULT NULL,
+  `telefono` varchar(20) DEFAULT NULL,
+  `correo` varchar(128) DEFAULT NULL,
+  `direccion` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `proveedor`
+--
+
+INSERT INTO `proveedor` (`id`, `nombre`, `num_documento`, `telefono`, `correo`, `direccion`) VALUES
+(3, 'HP Medical', '7894560225014', '2 2410803', 'comprasonline@hpmedical.com.bo', 'Calle, Belisario Salinas # 600, La Paz');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `rol`
+--
+
+CREATE TABLE `rol` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `rol`
+--
+
+INSERT INTO `rol` (`id`, `nombre`) VALUES
+(1, 'Gerente'),
+(2, 'Administrador'),
+(3, 'Almacenero'),
+(4, 'Vendedor');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id` int(11) NOT NULL,
+  `id_rol` int(11) DEFAULT NULL,
+  `nombre` varchar(45) NOT NULL,
+  `apellido` varchar(45) NOT NULL,
+  `num_documento` varchar(45) NOT NULL,
+  `telefono` varchar(20) NOT NULL,
+  `direccion` varchar(255) NOT NULL,
+  `correo` varchar(125) NOT NULL,
+  `contrasena` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`id`, `id_rol`, `nombre`, `apellido`, `num_documento`, `telefono`, `direccion`, `correo`, `contrasena`) VALUES
+(1, 3, 'Jean Marco', 'Gutiérrez Espejo', '9986683', '76206161', 'Calle Calatayud y huyustus', 'jean.gutierrez@gmail.com', '123456'),
+(2, 2, 'Marco Antonio', 'Pacari Alegría', '9245690', '69840923', 'Zona Ciudadela', 'marco.pacari@gmail.com', '123456'),
+(3, 2, 'Gustavo', 'Mamani', '456', '76230230', '123 Main St', 'carlos.mamani@gmail.com', 'password123');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `venta`
+--
+
+CREATE TABLE `venta` (
+  `id` int(11) NOT NULL,
+  `id_cliente` int(11) DEFAULT NULL,
+  `id_usuario` int(11) DEFAULT NULL,
+  `num_comprobante` varchar(45) DEFAULT NULL,
+  `fecha` date DEFAULT NULL,
+  `total` decimal(11,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `alquiler`
+--
+ALTER TABLE `alquiler`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `num_comprobante_UNIQUE` (`num_comprobante`),
+  ADD KEY `FK_alquiler_cliente_idx` (`id_cliente`),
+  ADD KEY `FK_alquiler_usuario_idx` (`id_usuario`);
+
+--
+-- Indices de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nombre_UNIQUE` (`nombre`);
+
+--
+-- Indices de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `num_documento_UNIQUE` (`num_documento`);
+
+--
+-- Indices de la tabla `compra`
+--
+ALTER TABLE `compra`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `num_comprobante_UNIQUE` (`num_comprobante`),
+  ADD KEY `FK_compra_proveedor_idx` (`id_proveedor`),
+  ADD KEY `FK_compra_usuario_idx` (`id_usuario`);
+
+--
+-- Indices de la tabla `detalle_alquiler`
+--
+ALTER TABLE `detalle_alquiler`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_det_alquiler_alquiler_idx` (`id_alquiler`),
+  ADD KEY `FK_det_alquiler_producto_idx` (`id_producto`);
+
+--
+-- Indices de la tabla `detalle_compra`
+--
+ALTER TABLE `detalle_compra`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_det_compra_compra_idx` (`id_compra`),
+  ADD KEY `FK_det_compra_producto_idx` (`id_producto`);
+
+--
+-- Indices de la tabla `detalle_venta`
+--
+ALTER TABLE `detalle_venta`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_det_venta_venta_idx` (`id_venta`),
+  ADD KEY `FK_det_venta_producto_idx` (`id_producto`);
+
+--
+-- Indices de la tabla `pais`
+--
+ALTER TABLE `pais`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_producto_categoria_idx` (`id_categoria`),
+  ADD KEY `FK_producto_pais` (`id_pais`);
+
+--
+-- Indices de la tabla `proveedor`
+--
+ALTER TABLE `proveedor`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `num_documento_UNIQUE` (`num_documento`);
+
+--
+-- Indices de la tabla `rol`
+--
+ALTER TABLE `rol`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `num_documento_UNIQUE` (`num_documento`),
+  ADD KEY `FK_usuario_rol_idx` (`id_rol`);
+
+--
+-- Indices de la tabla `venta`
+--
+ALTER TABLE `venta`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `num_comprobante_UNIQUE` (`num_comprobante`),
+  ADD KEY `FK_venta_cliente_idx` (`id_cliente`),
+  ADD KEY `FK_venta_usuario_idx` (`id_usuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `alquiler`
+--
+ALTER TABLE `alquiler`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT de la tabla `compra`
+--
+ALTER TABLE `compra`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `detalle_alquiler`
+--
+ALTER TABLE `detalle_alquiler`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `detalle_compra`
+--
+ALTER TABLE `detalle_compra`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `detalle_venta`
+--
+ALTER TABLE `detalle_venta`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `pais`
+--
+ALTER TABLE `pais`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `producto`
+--
+ALTER TABLE `producto`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- AUTO_INCREMENT de la tabla `proveedor`
+--
+ALTER TABLE `proveedor`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `rol`
+--
+ALTER TABLE `rol`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+
+--
+-- AUTO_INCREMENT de la tabla `venta`
+--
+ALTER TABLE `venta`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `alquiler`
+--
+ALTER TABLE `alquiler`
+  ADD CONSTRAINT `FK_alquiler_cliente` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_alquiler_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `compra`
+--
+ALTER TABLE `compra`
+  ADD CONSTRAINT `FK_compra_proveedor` FOREIGN KEY (`id_proveedor`) REFERENCES `proveedor` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_compra_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `detalle_alquiler`
+--
+ALTER TABLE `detalle_alquiler`
+  ADD CONSTRAINT `FK_det_alquiler_alquiler` FOREIGN KEY (`id_alquiler`) REFERENCES `alquiler` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_det_alquiler_producto` FOREIGN KEY (`id_producto`) REFERENCES `producto` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `detalle_compra`
+--
+ALTER TABLE `detalle_compra`
+  ADD CONSTRAINT `FK_det_compra_compra` FOREIGN KEY (`id_compra`) REFERENCES `compra` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_det_compra_producto` FOREIGN KEY (`id_producto`) REFERENCES `producto` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `detalle_venta`
+--
+ALTER TABLE `detalle_venta`
+  ADD CONSTRAINT `FK_det_venta_producto` FOREIGN KEY (`id_producto`) REFERENCES `producto` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_det_venta_venta` FOREIGN KEY (`id_venta`) REFERENCES `venta` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD CONSTRAINT `FK_producto_categoria` FOREIGN KEY (`id_categoria`) REFERENCES `categoria` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_producto_pais` FOREIGN KEY (`id_pais`) REFERENCES `pais` (`id`);
+
+--
+-- Filtros para la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD CONSTRAINT `FK_usuario_rol` FOREIGN KEY (`id_rol`) REFERENCES `rol` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `venta`
+--
+ALTER TABLE `venta`
+  ADD CONSTRAINT `FK_venta_cliente` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_venta_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
